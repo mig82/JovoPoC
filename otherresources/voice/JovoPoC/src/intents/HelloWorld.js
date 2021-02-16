b843514439143ad8d00078f5af61b3cfdc244d10
mig82 <miguelangelxfm@gmail.com>
2021-02-16 15:39:18 +0100

    Call Fabric anonymous service to say hello world
    This is still rudimentary because it requires the developer to throw a couple of lines of code to get the SDK instance
    from the Jovo $session and invoke the service.

--- otherresources/voice/JovoPoC/src/intents/HelloWorld.js
+++ otherresources/voice/JovoPoC/src/intents/HelloWorld.js
@@ -1,5 +1,9 @@
-function HelloWorld() {
-	this.ask("Hello World! What's your name?", 'Please tell me your name.');
+async function HelloWorld() {
+	let sdk = this.$session.$data.sdk
+	let service = sdk.getIntegrationService("HelloWorld")
+	let greeting = await service.invokeOperation("greet")
+	//this.ask("Hello World! What's your name?", 'Please tell me your name.')
+	this.ask(greeting.message + " What's your name?", 'Please tell me your name.')
 }
 
 module.exports = HelloWorld

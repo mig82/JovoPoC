00999f1f28b18ffdf0ae4d5c38e97c6ae84b6801
mig82 <miguelangelxfm@gmail.com>
2021-04-05 15:34:23 +0200

    Move Google account linking to Jovo project settings
    This avoids having to version so much of the platforms folder.

--- otherresources/voice/JovoPoC/project.js
+++ otherresources/voice/JovoPoC/project.js
@@ -3,11 +3,39 @@
 // ------------------------------------------------------------------
 
 module.exports = {
-  alexaSkill: {
-    nlu: 'alexa',
-  },
-  googleAction: {
-    projectId: 'jovopoc',
-  },
-  endpoint: '${JOVO_WEBHOOK_URL}',
+	alexaSkill: {
+		nlu: 'alexa',
+	},
+	googleAction: {
+		projectId: 'jovopoc',
+		manifest: {
+			settings: {
+				defaultLocale: 'en-US',
+				localizedSettings: {
+					'en-US': {
+						displayName: 'The Iron Bank of Braavos',
+						pronunciation: 'the iron bank of braavos',
+						developerEmail: 'miguel.fernandez@temenos.com',
+						developerName: 'Miguelángel Fernández',
+						fullDescription: "The Iron Bank's voice banking experience, with access to accounts, cards, personal finance mangement and more.",
+						privacyPolicyUrl: '',
+						shortDescription: "The Iron Bank's voice banking experience.",
+						smallLogoImage: 'https://quantum-demos1.s3.eu-central-1.amazonaws.com/ibb/logo_blue.png',
+						termsOfServiceUrl: ''
+					}
+				},
+				accountLinking: {
+					appClientId: 'jovopoc',
+					authGrantType: 'AUTH_CODE',
+					authorizationUrl: 'https://100032668.auth.konycloud.com/oidc/authorize/IronBankOauth2',
+					linkingType: 'OAUTH',
+					scopes: ['openid', 'email', 'profile', 'phone', 'offline_access'],
+					tokenUrl: 'https://tpko-dev.konycloud.com/services/Oauth2Token/token',
+					useBasicAuthHeader: true,
+				},
+				testingInstructions: "user@foo.com\nPw!123456"
+			}
+		},
+	},
+	endpoint: '${JOVO_WEBHOOK_URL}',
 };

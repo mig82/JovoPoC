31fa05a43ba791212d8936cc7eb4a26c0de40ca7
mig82 <miguelangelxfm@gmail.com>
2021-04-06 17:02:46 +0200

    Flesh out the help intent
    This is still WIP.

--- otherresources/voice/JovoPoC/src/intents/Help/index.js
+++ otherresources/voice/JovoPoC/src/intents/Help/index.js
@@ -1,7 +1,22 @@
 const kony = require('kony-node')
 
-function Help(){
-	kony.log("Help")
+const entity = "Iron Bank"
+
+function HelpIntent(){
+	kony.debug("Help")
 	//TODO: Push event to Fabric analytics.
+
+	this.$speech.addText("We understand you need help. That's ok. That's what we're here for.")
+	.addBreak('500ms')
+	.addText(
+		`The ${entity}'s voice banking experience allows you to access ` +
+		"several services from your virtual assistant. If you wish to " +
+		"check your accounts, loans, cards and other products, " +
+		"you'll have to link your account and sign in. If you just want to ask " +
+		"about other non-personal services, such as exchange rates or financial news, " +
+		"you may choose to remain anonymous."
+	)
+	this.tell(this.$speech)
+	this.toNext()
 }
-module.exports = Help
+module.exports = HelpIntent

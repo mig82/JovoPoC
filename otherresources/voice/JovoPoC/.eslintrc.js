be60e787316b9d180f1e55401c391392273deca2
mig82 <miguelangelxfm@gmail.com>
2021-07-23 18:28:04 +0200

    Configure VS Code debugging and EsLint

---
+++ otherresources/voice/JovoPoC/.eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+	"env": {
+		"browser": true,
+		"commonjs": true,
+		"es6": true
+	},
+	"extends": "eslint:recommended",
+	"globals": {
+		"Atomics": "readonly",
+		"SharedArrayBuffer": "readonly"
+	},
+	"parserOptions": {
+		"ecmaVersion": 2018
+	},
+	"rules": {
+		"indent": [
+			"warn",
+			"tab"
+		],
+		"linebreak-style": [
+			"error",
+			"unix"
+		]
+	}
+}

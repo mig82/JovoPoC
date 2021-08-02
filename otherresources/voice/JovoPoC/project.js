e9eee887a7efb480ba63cce08935cc40ce34a94e
mig82 <miguelangelxfm@gmail.com>
2021-08-02 20:05:24 +0200

    Use stages to build both Dialogflow and GActions
    The project.js file only allows for exporting a single `googleActions`
    property. Since we use Dialogflow for the chatbot model, we need this
    property to specify the Dialogflow project. However, we also need it to
    specify the GActions project. Using stages we can build both.

--- otherresources/voice/JovoPoC/project.js
+++ otherresources/voice/JovoPoC/project.js
@@ -4,72 +4,89 @@
 
 //TODO: Load all of this from a JSON file so that it can be easily written into by Visualizer.
 module.exports = {
-	alexaSkill: {
-		nlu: 'alexa',
-		manifest: {
-			publishingInformation: {
-				locales: {
-					"en-US": {
-						summary: "The Iron Bank of Braavos",
-						examplePhrases: [
-							"Alexa open Iron Bank"
-						],
-						keywords: [
-							"banking",
-							"financial services"
-						],
-						name: "Iron Bank",
-						description: "The Iron Bank's voice banking experience, with access to accounts, cards, personal finance mangement and more.",
-						smallIconUri: "https://quantum-demos1.s3.eu-central-1.amazonaws.com/ibb/logo_blue.png",
-						largeIconUri: "https://quantum-demos1.s3.eu-central-1.amazonaws.com/ibb/logo_blue.png"
+	endpoint: '${JOVO_WEBHOOK_URL}',
+
+	stages: {
+		//jovo build --stage dev_alexa
+		dev_alexa: {
+			//Amazon Alexa.
+			alexaSkill: {
+				nlu: 'alexa',
+				manifest: {
+					publishingInformation: {
+						locales: {
+							"en-US": {
+								summary: "The Iron Bank of Braavos",
+								examplePhrases: [
+									"Alexa open Iron Bank"
+								],
+								keywords: [
+									"banking",
+									"financial services"
+								],
+								name: "Iron Bank",
+								description: "The Iron Bank's voice banking experience, with access to accounts, cards, personal finance mangement and more.",
+								smallIconUri: "https://quantum-demos1.s3.eu-central-1.amazonaws.com/ibb/logo_blue.png",
+								largeIconUri: "https://quantum-demos1.s3.eu-central-1.amazonaws.com/ibb/logo_blue.png"
+							}
+						},
+						isAvailableWorldwide: true,
+						testingInstructions: "Test username: user@foo.com\nTest password: Pw!123456",
+						category: "BUSINESS_AND_FINANCE",
+						distributionCountries: []
+					}
+				}
+			}
+		},
+
+		//jovo build --stage dev_gactions
+		dev_gactions: {
+			//Google Assistant using Actions on Google.
+			googleAction: {
+				projectId: 'jovopoc',
+				manifest: {
+					settings: {
+						defaultLocale: 'en-US',
+						localizedSettings: {
+							'en-US': {
+								displayName: 'The Iron Bank of Braavos',
+								pronunciation: 'the iron bank of braavos',
+								developerEmail: 'miguel.fernandez@temenos.com',
+								developerName: 'Miguelángel Fernández',
+								fullDescription: "The Iron Bank's voice banking experience, with access to accounts, cards, personal finance mangement and more.",
+								privacyPolicyUrl: '',
+								shortDescription: "The Iron Bank's voice banking experience.",
+								// smallLogoImage: 'https://quantum-demos1.s3.eu-central-1.amazonaws.com/ibb/logo_blue.png',
+								termsOfServiceUrl: ''
+							}
+						},
+						accountLinking: {
+							appClientId: 'jovopoc',
+							authGrantType: 'AUTH_CODE',
+							authorizationUrl: 'https://100032668.auth.konycloud.com/oidc/authorize/IronBankOauth2',
+							linkingType: 'OAUTH',
+							scopes: ['openid', 'email', 'profile', 'phone', 'offline_access'],
+							tokenUrl: 'https://tpko-dev.konycloud.com/services/Oauth2Token/token',
+							useBasicAuthHeader: true,
+						},
+						testingInstructions: "Test username: user@foo.com\nTest password: Pw!123456"
 					}
 				},
-				isAvailableWorldwide: true,
-				testingInstructions: "Test username: user@foo.com\nTest password: Pw!123456",
-				category: "BUSINESS_AND_FINANCE",
-				distributionCountries: []
+			}
+		},
+
+		//jovo build --stage dev_dialogflow
+		dev_dialogflow: {
+			// This is a hack to be able to build the Dialogflow model, 
+			// even though we're only using it as the NLU for chatbots 
+			// while we also use Actions on Google for Google Assistant.
+			googleAction: {
+				nlu: 'dialogflow',
+				dialogflow: {
+					projectId: 'jovopoc-p9by',
+					keyFile: './secure/dialogflow_service_acct_key.json'
+				}
 			}
 		}
-	},
-
-	googleAction: {
-		nlu: 'dialogflow',
-		dialogflow: {
-			projectId: 'jovopoc-p9by',
-			keyFile: './secure/dialogflow_service_acct_key.json'
-		}
-	},
-	
-	// googleAction: {
-	// 	projectId: 'jovopoc',
-	// 	manifest: {
-	// 		settings: {
-	// 			defaultLocale: 'en-US',
-	// 			localizedSettings: {
-	// 				'en-US': {
-	// 					displayName: 'The Iron Bank of Braavos',
-	// 					pronunciation: 'the iron bank of braavos',
-	// 					developerEmail: 'miguel.fernandez@temenos.com',
-	// 					developerName: 'Miguelángel Fernández',
-	// 					fullDescription: "The Iron Bank's voice banking experience, with access to accounts, cards, personal finance mangement and more.",
-	// 					privacyPolicyUrl: '',
-	// 					shortDescription: "The Iron Bank's voice banking experience.",
-	// 					// smallLogoImage: 'https://quantum-demos1.s3.eu-central-1.amazonaws.com/ibb/logo_blue.png',
-	// 					termsOfServiceUrl: ''
-	// 				}
-	// 			},
-	// 			accountLinking: {
-	// 				appClientId: 'jovopoc',
-	// 				authGrantType: 'AUTH_CODE',
-	// 				authorizationUrl: 'https://100032668.auth.konycloud.com/oidc/authorize/IronBankOauth2',
-	// 				linkingType: 'OAUTH',
-	// 				scopes: ['openid', 'email', 'profile', 'phone', 'offline_access'],
-	// 				tokenUrl: 'https://tpko-dev.konycloud.com/services/Oauth2Token/token',
-	// 				useBasicAuthHeader: true,
-	// 			},
-	// 			testingInstructions: "Test username: user@foo.com\nTest password: Pw!123456"
-	// 		}
-	// 	},
-	// },
-	endpoint: '${JOVO_WEBHOOK_URL}',
-};
+	}
+}
